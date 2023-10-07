9ab70cd4522eaf42639cd01d000bc6eb27cd4a9e
Timoshchenko Nikolay <56084697+K0nek0@users.noreply.github.com>
2023-10-07 15:29:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ X = scaler.fit_transform(X)
 y = train_data["total_target"]
 
 # Разделение данных на тренировочный и тестовый наборы
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=len(test_data)/len(train_data), random_state=42)
 
 # Создание и обучение модели логистической регрессии
 model = LogisticRegression(max_iter=1000)
